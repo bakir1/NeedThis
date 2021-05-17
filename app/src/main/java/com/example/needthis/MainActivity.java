305b9e37d8329a128f6d588924f63d59b0a4c5e1
bakir1 <bakir_cutuk@hotmail.com>
2021-05-18 00:20:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/needthis/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.needthis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        /*        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+
+ */
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser == null){
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            finish();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/needthis/SignUpActivity.java
@@ -0,0 +1,62 @@
+package com.example.needthis;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailSignUp, passSignUp;
+    public Button signUpBtn;
+    public TextView signINText;
+    private FirebaseAuth auth;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailSignUp = findViewById(R.id.sign_up_email);
+        passSignUp = findViewById(R.id.sign_up_pass);
+        signUpBtn = findViewById(R.id.sign_up_btn);
+        signINText = findViewById(R.id.sign_in_text);
+
+        signINText.setOnClickListener(v -> startActivity(new Intent(SignUpActivity.this, LoginActivity.class)));
+
+        signUpBtn.setOnClickListener(v -> {
+            String email = emailSignUp.getText().toString();
+            String pass = passSignUp.getText().toString();
+
+            if (!email.isEmpty() && !pass.isEmpty()){
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                        finish();
+                    }else {
+                        Toast.makeText(SignUpActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else {
+                Toast.makeText(SignUpActivity.this, "Please Input All Fields!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
